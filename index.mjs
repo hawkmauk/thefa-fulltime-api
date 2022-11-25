e90a172e0a0e2cc26c99af6a01025700ca49e3a7
Michael Hawkins <michael.hawkins@netcall.com>
2022-11-25 09:37:41 +0000

    published @hawkmauk/thefa-fulltime-api:1.0.0 npm

--- index.mjs
+++ index.mjs
@@ -9,7 +9,7 @@ teams( data.league.id, data.season.id, data.club.id )
                 fixtures( data.season.id, team.id )
                     .then( async fixtures => {
                         fixtures.forEach( fixture => {
-                            if ( team.name == fixture.home ) console.log( `${ team.name } are playing at home on ${ fixture.date } against ${ fixture.away }` )
+                            if ( team.name == fixture.home ) console.log( `${ team.name.replace( data.club.name, '').trim() } are playing at home on ${ fixture.date } against ${ fixture.away }` )
                         })
                     })
                 })
